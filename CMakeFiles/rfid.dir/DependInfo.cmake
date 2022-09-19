a446303b16c5db8b83d31966f6785fdf6345a551
Ubuntu <muk-e muk0403@naver.com>
2022-09-19 16:18:51 +0900

    first commit

---
+++ CMakeFiles/rfid.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/project/GolfMembersManager/src/hw/rfid/mfrc522.cpp" "src/CMakeFiles/rfid.dir/hw/rfid/mfrc522.cpp.o" "gcc" "src/CMakeFiles/rfid.dir/hw/rfid/mfrc522.cpp.o.d"
+  "/home/ubuntu/project/GolfMembersManager/src/hw/spi/SPI.cpp" "src/CMakeFiles/rfid.dir/hw/spi/SPI.cpp.o" "gcc" "src/CMakeFiles/rfid.dir/hw/spi/SPI.cpp.o.d"
+  "/home/ubuntu/project/GolfMembersManager/src/main.cpp" "src/CMakeFiles/rfid.dir/main.cpp.o" "gcc" "src/CMakeFiles/rfid.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/rfid.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rfid.dir/hw/rfid/mfrc522.cpp.o"
+  "CMakeFiles/rfid.dir/hw/rfid/mfrc522.cpp.o.d"
+  "CMakeFiles/rfid.dir/hw/spi/SPI.cpp.o"
+  "CMakeFiles/rfid.dir/hw/spi/SPI.cpp.o.d"
+  "CMakeFiles/rfid.dir/main.cpp.o"
+  "CMakeFiles/rfid.dir/main.cpp.o.d"
+  "rfid"
+  "rfid.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rfid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
